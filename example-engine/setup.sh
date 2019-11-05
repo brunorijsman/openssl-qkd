9dade7d0fccb53f7eabff797428000101b6dffe2
Yvo Keuter <yvo.keuter@ebpi.nl>
2019-11-05 15:08:28 +0000

    listing the engine

--- example-engine/setup.sh
+++ example-engine/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
+export OPENSSL_CONF="/workspaces/ripe-quantum-hackathon-nov-2019/example-engine/openssl.cnf"
--- example-engine/setup_ubuntu.sh
+++ example-engine/setup_ubuntu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="${HOME}/openssl/:."
+export LD_LIBRARY_PATH="$HOME/openssl/:."
+export OPENSSL_CONF="/workspaces/ripe-quantum-hackathon-nov-2019/example-engine/openssl.cnf"
