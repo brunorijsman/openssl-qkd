1f8e1a02f8d53f2e52ec571ead06f0e7470f1867
Yvo Keuter <yvo.keuter@ebpi.nl>
2019-11-06 13:03:02 +0000

    split config

--- etsi-qkd-engine/setup_client.sh
+++ etsi-qkd-engine/setup_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
-export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/example-engine/openssl.cnf"
+export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/etsi-qkd-engine/openssl_server.cnf"
 export OPENSSL_ENGINES="/usr/local/lib/engines-3"
---
+++ etsi-qkd-engine/setup_server_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="$HOME/openssl/:."
+export OPENSSL_CONF="/workspaces/ripe-quantum-hackathon-nov-2019/etsi-qkd-engine/openssl_server.cnf"
+export OPENSSL_ENGINES="/usr/local/lib/engines-3"
