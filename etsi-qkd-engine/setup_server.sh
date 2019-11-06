7b86ce87bd8f686ab64a60e2622a191ce62a6d4b
Bruno Rijsman <brunorijsman@gmail.com>
2019-11-06 14:25:42 +0100

    Update setup script

--- etsi-qkd-engine/setup_server.sh
+++ etsi-qkd-engine/setup_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ ! -L /usr/local/lib/engine/etsi_qkd_server.dylib ]]; then
+    ln -s etsi_qkd_server.dylib /usr/local/lib/engine/etsi_qkd_server.dylib 
+fi
+
+if [[ ! -L /usr/local/lib/engine/etsi_qkd_client.dylib ]]; then
+    ln -s etsi_qkd_client.dylib /usr/local/lib/engine/etsi_qkd_client.dylib 
+fi
+
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
 export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/etsi-qkd-engine/openssl_server.cnf"
 export OPENSSL_ENGINES="/usr/local/lib/engines-3"
