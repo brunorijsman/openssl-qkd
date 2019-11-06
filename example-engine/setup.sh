9b4d6298f3894e2939bfae64f610e3b90be2c8f0
Bruno Rijsman <brunorijsman@gmail.com>
2019-11-06 09:07:42 +0100

    Start work on fixed keys

--- example-engine/setup.sh
+++ example-engine/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
-export OPENSSL_CONF="/workspaces/ripe-quantum-hackathon-nov-2019/example-engine/openssl.cnf"
+export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/example-engine/openssl.cnf"
