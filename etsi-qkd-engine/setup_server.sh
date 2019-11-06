46ef336c1e420337ada23c4d1326e60614bf9d18
Bruno Rijsman <brunorijsman@gmail.com>
2019-11-06 14:26:44 +0100

    Fix Max engines dir

--- etsi-qkd-engine/setup_server.sh
+++ etsi-qkd-engine/setup_server.sh
@@ -10,4 +10,4 @@ fi
 
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
 export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/etsi-qkd-engine/openssl_server.cnf"
-export OPENSSL_ENGINES="/usr/local/lib/engines-3"
+export OPENSSL_ENGINES="/usr/local/lib/engine"
