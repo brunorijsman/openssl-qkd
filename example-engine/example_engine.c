6dd22c48bb78e0fbc8f59abc81022a1bbcfe33b6
Bruno Rijsman <brunorijsman@gmail.com>
2019-11-06 12:30:43 +0100

    Try fixed num

--- example-engine/example_engine.c
+++ example-engine/example_engine.c
@@ -76,70 +76,70 @@ static int dh_generate_key(DH *dh)
 {
     printf("dh_generate_key [enter]\n");
     
-    // BIGNUM *pub_key = BN_secure_new();
-    // report_progress("dh_generate_key: BN_secure_new (priv_key)", pub_key != NULL);
+    BIGNUM *pub_key = BN_secure_new();
+    report_progress("dh_generate_key: BN_secure_new (priv_key)", pub_key != NULL);
 
-    // BIGNUM *priv_key = BN_secure_new();
-    // report_progress("dh_generate_key: BN_secure_new (pub_key)", priv_key != NULL);
+    BIGNUM *priv_key = BN_secure_new();
+    report_progress("dh_generate_key: BN_secure_new (pub_key)", priv_key != NULL);
 
-    // if (return_fixed_key_for_testing) {
-    //     printf("dh_generate_key (fixed number)\n");
-    //     BN_set_word(priv_key, 11111);
-    //     BN_set_word(pub_key, 22222);
-    // } else if (i_am_server()) {
-    //     printf("dh_generate_key (server)\n");
-    //     /* Set key_handle to "NULL" key handle, to request QKD_OPEN to allocate a key handle. */
-    //     key_handle_t key_handle = {0};
-    //     /* TODO: For now, the stub has a hard-coded assumption that the QKD server and client run
-    //        on the same host, and the stub never even looks at the destination address. */
-    //     ip_address_t destination = {
-    //         .length = 0,
-    //         .address = {0}
-    //     };
-    //     /* TODO: For now, set QoS to dummy values */
-    //     qos_t qos = {
-    //         .requested_length = 0,   /* TODO: This one we should probably set */
-    //         .max_bps = 0,
-    //         .priority = 0,
-    //         .timeout = 0
-    //     };
-    //     enum RETURN_CODES result;
-    //     result = QKD_OPEN(destination, qos, key_handle);
-    //     report_progress("QKD_OPEN", QKD_RC_SUCCESS == result);
+    if (return_fixed_key_for_testing) {
+        printf("dh_generate_key (fixed number)\n");
+        BN_set_word(priv_key, 11111);
+        BN_set_word(pub_key, 22222);
+    } else if (i_am_server()) {
+        printf("dh_generate_key (server)\n");
+        /* Set key_handle to "NULL" key handle, to request QKD_OPEN to allocate a key handle. */
+        key_handle_t key_handle = {0};
+        /* TODO: For now, the stub has a hard-coded assumption that the QKD server and client run
+           on the same host, and the stub never even looks at the destination address. */
+        ip_address_t destination = {
+            .length = 0,
+            .address = {0}
+        };
+        /* TODO: For now, set QoS to dummy values */
+        qos_t qos = {
+            .requested_length = 0,   /* TODO: This one we should probably set */
+            .max_bps = 0,
+            .priority = 0,
+            .timeout = 0
+        };
+        enum RETURN_CODES result;
+        result = QKD_OPEN(destination, qos, key_handle);
+        report_progress("QKD_OPEN", QKD_RC_SUCCESS == result);
 
-    //     /* TODO: Server side processing
-    //        - The server calls QKD_OPEN() with a NULL key_handle, which generates a new
-    //          key_handle, which is 64 octets.   **DONE***
-    //        - For now, we only specify the requested_lengh as a QoS parameter. (How many bytes of
-    //          shared secret does DH require?). For now, we don't specify max_bps or priority.
-    //        - What timeout shall we use? Is OpenSSL tolerant to this function blocking for even a few
-    //          ms? What happens is we specify a timeout of 0 (zero)?
-    //        - The server uses the key_handle as the DH public key. This public key will be included
-    //          in the Server-Hello message which plays the role of the SEND_KEY_HANDLE() operation in
-    //          the ETSI sequence diagram. Note that this is why the ETSI document requires that "no
-    //          key material can be derived from the handle" (top of page 9).
-    //        - The server sets the DH private key to NULL. The QKD exchange does not use any DH
-    //          private key on the server. */
-    // } else {
-    //     printf("dh_generate_key (client)\n");
-    //     /* TODO: Client side processing
-    //        - The client uses the received DH public key as the ETSI API key_handle
-    //        - The client calls QKD_OPEN() with the key_handle obtained it this way.
-    //        - Note that the overloaded generate_key function does different things on the server
-    //          side and the client side. Thus, there needs to be some way for the overloaded function
-    //          to "know" whether it is being called in a server role or a client role. (How?)
-    //        - The client uses the key_handle as it's own DH public key. In other words, the client's
-    //          DH public key is the same as the server's DH public key.
-    //        - The client sets its DH private key to NULL. Just as on the server, the QKD exchange
-    //          does not use any DH private key on the client. */
-    // }
-    // /* I am not sure if we can synchronize the key_handle in this function already.
-    //    We could calculate the 'lowest slot' on both sides of the link here
-    //    and then agree on a key_handle in the compute function by taking the max. 
-    //    That would mean no qkd api calls here.*/
+        /* TODO: Server side processing
+           - The server calls QKD_OPEN() with a NULL key_handle, which generates a new
+             key_handle, which is 64 octets.   **DONE***
+           - For now, we only specify the requested_lengh as a QoS parameter. (How many bytes of
+             shared secret does DH require?). For now, we don't specify max_bps or priority.
+           - What timeout shall we use? Is OpenSSL tolerant to this function blocking for even a few
+             ms? What happens is we specify a timeout of 0 (zero)?
+           - The server uses the key_handle as the DH public key. This public key will be included
+             in the Server-Hello message which plays the role of the SEND_KEY_HANDLE() operation in
+             the ETSI sequence diagram. Note that this is why the ETSI document requires that "no
+             key material can be derived from the handle" (top of page 9).
+           - The server sets the DH private key to NULL. The QKD exchange does not use any DH
+             private key on the server. */
+    } else {
+        printf("dh_generate_key (client)\n");
+        /* TODO: Client side processing
+           - The client uses the received DH public key as the ETSI API key_handle
+           - The client calls QKD_OPEN() with the key_handle obtained it this way.
+           - Note that the overloaded generate_key function does different things on the server
+             side and the client side. Thus, there needs to be some way for the overloaded function
+             to "know" whether it is being called in a server role or a client role. (How?)
+           - The client uses the key_handle as it's own DH public key. In other words, the client's
+             DH public key is the same as the server's DH public key.
+           - The client sets its DH private key to NULL. Just as on the server, the QKD exchange
+             does not use any DH private key on the client. */
+    }
+    /* I am not sure if we can synchronize the key_handle in this function already.
+       We could calculate the 'lowest slot' on both sides of the link here
+       and then agree on a key_handle in the compute function by taking the max. 
+       That would mean no qkd api calls here.*/
 
-    // int result = DH_set0_key(dh, pub_key, priv_key);
-    // report_progress("dh_generate_key: DH_set0_key", result == 1);
+    int result = DH_set0_key(dh, pub_key, priv_key);
+    report_progress("dh_generate_key: DH_set0_key", result == 1);
 
     printf("dh_generate_key [exit]\n");
     return 1;
@@ -180,8 +180,7 @@ static int dh_generate_key(DH *dh)
 static DH_METHOD example_dh_method = {
     .name = "Example DH Method",
     .generate_key = dh_generate_key,
-    .compute_key = NULL,
-    // TODO: put back .compute_key = dh_compute_key,
+    .compute_key = dh_compute_key,
     .bn_mod_exp = NULL,                     /* TODO */
     .init = NULL,                           /* TODO */
     .finish = NULL,                         /* TODO */
