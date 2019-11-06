aca396826332a7c4cbaaeb95e89e680fb324df0a
Yvo Keuter <yvo.keuter@ebpi.nl>
2019-11-06 14:30:29 +0000

    client implementation

--- etsi-qkd-engine/etsi_qkd_client.c
+++ etsi-qkd-engine/etsi_qkd_client.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <string.h>
 #include <openssl/engine.h>
 #include "qkd_api.h"
@@ -19,6 +20,8 @@ static int client_generate_key(DH *dh)
         BN_set_word(priv_key, 11111);
         BN_set_word(pub_key, 22222);
     } else {
+        BN_set_word(priv_key, 0);
+        BN_set_word(pub_key, 0);
         /* TODO: Client side processing
         - The client uses the received DH public key as the ETSI API key_handle
         - The client calls QKD_OPEN() with the key_handle obtained it this way.
@@ -42,6 +45,52 @@ static int client_compute_key(unsigned char *key, const BIGNUM *pub_key, DH *dh)
 {
     printf("client_compute_key [enter]\n");
     
+    // We get the key handle from the public key of the server.
+    key_handle_t key_handle;
+    int size = BN_bn2bin(pub_key, (unsigned char *)key_handle);
+    assert(size == KEY_HANDLE_SIZE);
+
+    printf("KEY HANDLE: ");
+    for (int i = 0; i < sizeof(key); i++) {
+        printf("%02x", (unsigned char) (key_handle[i]));
+    }
+
+    /* TODO: For now, the stub has a hard-coded assumption that the QKD server and client run
+             on the same host, and the stub never even looks at the destination address. */
+    ip_address_t destination = {
+            .length = 0,
+            .address = {0}
+    };
+        /* TODO: For now, set QoS to dummy values */
+    int key_size = DH_size(dh);
+    qos_t qos = {
+            .requested_length = key_size,   /* TODO: This one we should probably set */
+            .max_bps = 0,
+            .priority = 0,
+            .timeout = 0
+    };
+    enum RETURN_CODES result;
+    result = QKD_OPEN(destination, qos, key_handle);
+    report_progress("QKD_OPEN", QKD_RC_SUCCESS == result);
+
+    result = QKD_CONNECT_BLOCKING(key_handle, 0);
+    report_progress("QKD_CONNECT_BLOCKING", QKD_RC_SUCCESS == result);
+
+    printf("Allocated size=%d\n", key_size);
+    report_progress("client_compute_key: allocate shared secret memory", key != NULL);
+    // /* TODO: put somthing in the key */
+    // memset(key, 3, size);
+    result = QKD_GET_KEY(key_handle, (char *)key);
+    report_progress("QKD_GET_KEY", QKD_RC_SUCCESS == result);
+
+    printf("KEY: ");
+    for (int i = 0; i < sizeof(key); i++) {
+        printf("%02x", (unsigned char) (key[i]));
+    }
+
+    result = QKD_CLOSE(key_handle);
+    report_progress("QKD_CLOSE", QKD_RC_SUCCESS == result);
+
     /* TODO: The overloaded compute_key function on the client does the following:
     - The client calls QKD_CONNECT_BLOCKING.
     - This call requires the IP address of the peer (the server in this case). 
@@ -52,14 +101,11 @@ static int client_compute_key(unsigned char *key, const BIGNUM *pub_key, DH *dh)
     - The client calls QKD_GET_KEY which returns a key_buffer. This is used as 
       the shared secret and returned. */
 
-    int size = DH_size(dh);
-    printf("Allocated size=%d\n", size);
-    report_progress("client_compute_key: allocate shared secret memory", key != NULL);
-    /* TODO: put somthing in the key */
-    memset(key, 3, size);
+    
 
     printf("client_compute_key [exit]\n");
-    return size;
+
+    return key_size;
 }
 
 static DH_METHOD client_dh_method = {
