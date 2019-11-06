5554d40764d8bcac1a0c16b7fbeea251301614c9
Yvo Keuter <yvo.keuter@ebpi.nl>
2019-11-06 11:03:05 +0000

    config related

--- example-engine/setup.sh
+++ example-engine/setup.sh
@@ -2,3 +2,4 @@
 
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
 export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/example-engine/openssl.cnf"
+export OPENSSL_ENGINES="/usr/local/lib/engines-3"
