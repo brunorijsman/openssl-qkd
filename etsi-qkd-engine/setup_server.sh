765175a6e260bb8c4d2c72dd596b74cc2c327152
Bruno Rijsman <brunorijsman@gmail.com>
2019-11-06 14:28:29 +0100

    Fix scripts

--- etsi-qkd-engine/setup_server.sh
+++ etsi-qkd-engine/setup_server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [[ ! -L /usr/local/lib/engine/etsi_qkd_server.dylib ]]; then
-    ln -s etsi_qkd_server.dylib /usr/local/lib/engine/etsi_qkd_server.dylib 
+export OPENSSL_ENGINES="/usr/local/lib/engine"
+
+if [[ ! -L {$OPENSSL_ENGINES}/etsi_qkd_server.dylib ]]; then
+    ln -s etsi_qkd_server.dylib {$OPENSSL_ENGINES}/etsi_qkd_server.dylib 
 fi
 
-if [[ ! -L /usr/local/lib/engine/etsi_qkd_client.dylib ]]; then
-    ln -s etsi_qkd_client.dylib /usr/local/lib/engine/etsi_qkd_client.dylib 
+if [[ ! -L {$OPENSSL_ENGINES}/etsi_qkd_client.dylib ]]; then
+    ln -s etsi_qkd_client.dylib {$OPENSSL_ENGINES}/etsi_qkd_client.dylib 
 fi
 
 export DYLD_FALLBACK_LIBRARY_PATH="${HOME}/openssl/:."
 export OPENSSL_CONF="${HOME}/ripe-quantum-hackathon-nov-2019/etsi-qkd-engine/openssl_server.cnf"
-export OPENSSL_ENGINES="/usr/local/lib/engine"
--- etsi-qkd-engine/setup_server_ubuntu.sh
+++ etsi-qkd-engine/setup_server_ubuntu.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export OPENSSL_ENGINES="/usr/local/lib/engines-3"
+
+if [[ ! -L {$OPENSSL_ENGINES}/etsi_qkd_server.so ]]; then
+    ln -s etsi_qkd_server.dylib {$OPENSSL_ENGINES}/etsi_qkd_server.so 
+fi
+
+if [[ ! -L {$OPENSSL_ENGINES}/etsi_qkd_client.so ]]; then
+    ln -s etsi_qkd_client.dylib {$OPENSSL_ENGINES}/etsi_qkd_client.so
+fi
+
 export LD_LIBRARY_PATH="$HOME/openssl/:."
 export OPENSSL_CONF="/workspaces/ripe-quantum-hackathon-nov-2019/etsi-qkd-engine/openssl_server.cnf"
-export OPENSSL_ENGINES="/usr/local/lib/engines-3"
